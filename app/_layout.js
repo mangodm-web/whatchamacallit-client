fa633574ecf41a689ed2afc5ce2da49a7b846c6f
mangodm-web <mangodm.web3@gmail.com>
2023-08-28 07:10:31 +0900

    feat: 메인화면 root layout에 추가

--- app/_layout.js
+++ app/_layout.js
@@ -5,6 +5,7 @@ export default () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "", headerShown: false }} />
+      <Stack.Screen name="intro" options={{ title: "" }} />
     </Stack>
   );
 };
