4e34d98c034a11afae5bb3c046e57607e235fefe
mangodm-web <mangodm.web3@gmail.com>
2023-08-28 06:34:38 +0900

    feat: 질문/답변 샘플 데이터 추가

---
+++ constants/sampleQuestionAnswers.js
@@ -0,0 +1,36 @@
+export const SAMPLE_QUESTION_ANSWERS = [
+  {
+    id: 1,
+    description:
+      "A place in New York where lights and billboards everywhere...",
+    answer: "Times Square",
+  },
+  {
+    id: 2,
+    description:
+      "There's a grand palace from Joseon dynasty in Seoul. What's it called?",
+    answer: "Gyeongbokgung Palace",
+  },
+  {
+    id: 3,
+    description: "Official home, top U.S. guy. Right in the capital. It's...?",
+    answer: "White House",
+  },
+  {
+    id: 4,
+    description:
+      "Big waterfalls, between U.S. and Canada. So famous. What are they?",
+    answer: "Niagara Falls",
+  },
+  {
+    id: 5,
+    description: "That tower... doesn't stand right. In Europe. What's it?",
+    answer: "Leaning Tower of Pisa",
+  },
+  {
+    id: 6,
+    description:
+      "Huge wall, went on for miles and miles... protected places. In Asia. Called...?",
+    answer: "Great Wall of China",
+  },
+];
