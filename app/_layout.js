f4824cacd6cbf627013dfc5969bfc73c2c2794d1
mangodm-web <mangodm.web3@gmail.com>
2023-08-29 04:23:02 +0900

    feat: 정/오답 화면 root layout에 추가

--- app/_layout.js
+++ app/_layout.js
@@ -7,6 +7,7 @@ export default () => {
       <Stack.Screen name="index" options={{ title: "", headerShown: false }} />
       <Stack.Screen name="intro" options={{ title: "" }} />
       <Stack.Screen name="predictions" options={{ title: "" }} />
+      <Stack.Screen name="results" options={{ title: "" }} />
     </Stack>
   );
 };
