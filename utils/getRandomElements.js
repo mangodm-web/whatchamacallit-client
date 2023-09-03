32e62737ccca52bd390e363a303ccf8cc51926fb
mangodm-web <mangodm.web3@gmail.com>
2023-09-03 09:45:28 +0800

    feat: 입력값 유효성 검증 및 예외 처리 추가

--- utils/getRandomElements.js
+++ utils/getRandomElements.js
@@ -1,4 +1,25 @@
 export function getRandomElements(array, numberOfElementsToPick = 1) {
+  if (!Array.isArray(array)) {
+    throw new TypeError("The first argument must be an array.");
+  }
+  if (
+    typeof numberOfElementsToPick !== "number" ||
+    !Number.isInteger(numberOfElementsToPick)
+  ) {
+    throw new TypeError(
+      "The second argument should be a non-negative integer."
+    );
+  }
+  if (numberOfElementsToPick < 0) {
+    throw new RangeError(
+      "The second argument should be a non-negative integer."
+    );
+  }
+
+  if (array.length === 0 || numberOfElementsToPick === 0) {
+    return [];
+  }
+
   const results = new Set();
 
   while (results.size < numberOfElementsToPick) {
