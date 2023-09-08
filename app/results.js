978c86aacc0ac880c0ca9c77dacca41abc482ad9
mangodm-web <mangodm.web3@gmail.com>
2023-09-08 23:24:53 +0800

    chore: 이미지, 아이콘 관련 컴포넌트에 testID 추가

--- app/results.js
+++ app/results.js
@@ -31,6 +31,7 @@ export default function ResultsScreen() {
           <Image
             source={require("../assets/bot.png")}
             className={"w-32 h-32 mt-3"}
+            testID="bot-image"
           />
         </View>
         <View className="space-y-2 p-4 mt-1 bg-white border border-slate-300 rounded-lg">
@@ -41,7 +42,7 @@ export default function ResultsScreen() {
             onPress={() => router.push("/predictions")}
             className="w-40 p-3 mt-5 bg-gray-200 rounded-full"
           >
-            <Text className="text-center">
+            <Text className="text-center" testID="mic-icon">
               <FontAwesome5 name="microphone" size={24} color="black" />
             </Text>
           </Pressable>
