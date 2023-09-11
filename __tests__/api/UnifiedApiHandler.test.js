843ee9b59ed0cec34129d6807acac60c29845ce7
mangodm-web <mangodm.web3@gmail.com>
2023-09-11 20:47:45 +0900

    test: createPredictions, createFeedback 메소드 테스트 추가

--- __tests__/api/UnifiedApiHandler.test.js
+++ __tests__/api/UnifiedApiHandler.test.js
@@ -41,4 +41,48 @@ describe("UnifiedApiHandler", () => {
       "Audio is required."
     );
   });
+
+  it("should create a prediction", async () => {
+    const fakeDescription = "fake description";
+    const fakePrediction = "fake prediction";
+    const fakePredictionSuccessResponse = {
+      data: {
+        predictions: fakePrediction,
+      },
+    };
+
+    mock
+      .onPost(`/${CONFIG.API_STT_LATEST_VERSION}/predictions`)
+      .reply(200, fakePredictionSuccessResponse);
+
+    const result = await apiHandler.createPredictions(fakeDescription);
+    expect(result).toBe(fakePrediction);
+  });
+
+  it("should throw an error when description is missing", async () => {
+    await expect(apiHandler.createPredictions()).rejects.toThrow(
+      "Description is required."
+    );
+  });
+
+  it("should create a feedback", async () => {
+    const fakeFeedback = "fake feedback";
+    const fakeObjectId = "test_id";
+    const fakeFeedbackSuccessResponse = {
+      data: { _id: fakeObjectId },
+    };
+
+    mock
+      .onPost(`/${CONFIG.API_STT_LATEST_VERSION}/predictions/feedback`)
+      .reply(200, fakeFeedbackSuccessResponse);
+
+    const result = await apiHandler.createFeedback(fakeFeedback);
+    expect(result.data.data._id).toBe(fakeObjectId);
+  });
+
+  it("should throw an error when feedback is missing", async () => {
+    await expect(apiHandler.createFeedback()).rejects.toThrow(
+      "Feedback is required."
+    );
+  });
 });
